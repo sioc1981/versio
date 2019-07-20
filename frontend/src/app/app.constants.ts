37b39c083472a853d5715cdcefd9712079b2633d
sioc1981 <sioc1981@users.noreply.github.com>
2019-07-21 00:23:42 +0200

    fix for backend address

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const APP_CONSTANT = {
     applicationName: 'Version Management',
-    backendUrlBase: location.protocol + '//' + location.hostname + ':8080' + '/backend/api',
+    backendUrlBase: location.protocol + '//' + location.hostname + ':' + location.port + '/backend/api',
     httpOptions: {
         headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     }
